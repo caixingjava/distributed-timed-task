4ccc4471fdb9f07e243ab68db847f4cbad9babc0
caixing <18851418545@163.com>
2019-07-29 18:27:12 +0800

    初始化工程

---
+++ distributed-timed-task-api/src/main/java/com/distributed/timed/task/api/config/Config.java
@@ -0,0 +1,12 @@
+package com.distributed.timed.task.api.config;
+
+/**
+ * <Description> <br>
+ *
+ * @author caixing<br>
+ * @version 1.0<br>
+ * @taskId <br>
+ * @CreateDate 2019/7/29 18:19 <br>
+ */
+public class Config {
+}
